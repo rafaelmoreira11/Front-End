7f061edbf15f3867fd6ea28f7e1eb1caae32e032
Rafael Moreira <rafaelmoreiralima50@gmail.com>
2024-01-15 20:54:58 -0300

    7DoC

---
+++ 7DaysOfCode/Lógica JS/Dia1/assets/script.js
@@ -0,0 +1,32 @@
+let numUm = 1;
+let strUm = "1";
+
+let numTrinta = 30;
+let strTrinta = "30";
+
+let numDez = 10;
+let strDez = "10";
+
+if (numUm == strUm) {
+  console.log(
+    "As variáveis numUm e strUm tem o mesmo valor, mas tipos diferentes !"
+  );
+} else {
+  console.log("As variáveis numUm e strUm não tem o mesmo valor");
+}
+
+if (numTrinta == strTrinta) {
+  console.log(
+    "As variáveis numTrinta e strTrinta tem o mesmo valor, mas tipos diferentes !"
+  );
+} else {
+  console.log("As variáveis numTrinta e strTrinta não tem o mesmo valor");
+}
+
+if (numDez == strDez) {
+  console.log(
+    "As variáveis numDez e strDez tem o mesmo valor, mas tipos diferentes !"
+  );
+} else {
+  console.log("As variáveis numDez e strDez não tem o mesmo valor");
+}
